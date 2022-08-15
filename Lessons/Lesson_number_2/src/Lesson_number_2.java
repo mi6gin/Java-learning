3fe250250196945d139c8f1d2a6e542c7ec10fec
mi6gun <endlesspossibilitieskr@gmail.com>
2022-08-16 00:03:34 +0600

    Второе пришествие

---
+++ Lessons/Lesson_number_2/src/Lesson_number_2.java
@@ -0,0 +1,14 @@
+public class Lesson_number_2 {
+    public static void main(String[] args){
+        int jojo=1;
+        int dio=2;
+        jojo=count(jojo);
+        dio=count(dio);
+        System.out.println(jojo);
+        System.out.println(dio);
+    }
+    private static int count(int x){
+        return x=x+1;
+    }
+
+}
