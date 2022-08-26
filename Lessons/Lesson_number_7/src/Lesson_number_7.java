f26cac49d4303aa57e22072b9755f7130670351c
mi6gun <80161464+mi6gin@users.noreply.github.com>
2022-08-26 18:49:59 +0600

    PIPIN

---
+++ Lessons/Lesson_number_7/src/Lesson_number_7.java
@@ -0,0 +1,8 @@
+public class Lesson_number_7 {
+    public static void main(String[] args) {
+        int [] m = {1, 5, 9, 0, 11};
+        for(int x=0; x<5; x++){
+            System.out.println(-m[x]);
+        }
+    }
+}
---
+++ Lessons/Lesson_number_7/src/under_bee.java
@@ -0,0 +1,9 @@
+public class under_bee {
+    public static void main(String[] args) {
+        int [] m = new int[5];
+        for(int x=0; x<5; x++){
+            m[x]=-x;
+            System.out.println(m[x]);
+        }
+    }
+}
